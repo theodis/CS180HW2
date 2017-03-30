300e3ff3cb9f18a60d1cee3c14e5e2294c38ccc4
Mike Schmid <theodis3@gmail.com>
2017-03-30 01:23:50 -0700

    Added some base weights

---
+++ generateBaseWeights.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+pieceMap = {
+    '-' : 8,
+    'K' : 0,
+    'N' : 2,
+    'B' : 2,
+    'R' : 2,
+    'P' : 6, 
+    'k' : 16,
+    'n' : 14,
+    'b' : 14,
+    'r' : 14,
+    'p' : 10
+}
+
+pieceOrder = ['-','K','N','B','R','P','k','n','b','r','p']
+
+output = ""
+for piece in pieceOrder:
+    for j in range(8):
+        for i in range(6):
+            output += ("%04x" % pieceMap[piece]).upper()
+print(output)
